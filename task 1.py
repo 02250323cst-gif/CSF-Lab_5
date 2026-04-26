5a37395fc00fcd14d63089e6e3c87730a762481b
02250323.cst@rub.edu.bt <02250323.cst@rub.edu.bt>
2026-04-26 14:24:14 +0530

    Added test.python file

---
+++ task 1.py
@@ -0,0 +1,31 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        return "Stack is empty"
+
+    def peek(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        return "Stack is empty"
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def display(self):
+        print(self.stack)
+
+s = Stack()
+s.push(10)
+s.push(20)
+s.push(30)
+print("Stack:", s.stack)
+print("Top:", s.peek())
+print("Popped:", s.pop())
+print("After pop:", s.stack)
